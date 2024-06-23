03d048a988445ec8f7f9cb62d1b768cb7c8654ee
DimitrisTheo-Dev <ice20390073@uniwa.gr>
2024-06-23 23:08:01 +0300

    moved files

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello from the backend.");
+});
+
+
+// Start server
+const PORT = 5005;
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+});
